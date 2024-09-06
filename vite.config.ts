e605ea649ddac979cc28b995a7e92e2fd494c88c
Damon Vestervand <damon@vestervand.net>
2024-09-06 13:33:10 +0200

    configure adapter-node, sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "beyond-work-eu",
+            project: "bui"
+        }
+    }), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
